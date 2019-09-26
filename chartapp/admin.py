19f01e66c57dd9e569076ea61fe33cba01a4ac16
oleksii_nechytailo <o.nechytailo@wrenchtech.io>
2019-09-26 18:05:00 +0300

    add admin

--- chartapp/admin.py
+++ chartapp/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
 
 from chartapp.models import Product, Contact
 
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('My site admin')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('My administration')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Site administration')
 
 class ContactAdmin(admin.ModelAdmin):
     fields = ["last_name", "first_name"]
+    def response_change(self, request, obj):
+        pass
+
+
 
 
 admin.site.register(Contact, ContactAdmin)
