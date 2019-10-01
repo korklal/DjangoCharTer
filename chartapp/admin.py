12d12be3c3d5a23fb155578716326b910c5f4979
alx <korklal#gmail.com>
2019-10-01 08:04:27 +0300

    rf admin

--- chartapp/admin.py
+++ chartapp/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.utils.translation import ugettext_lazy
 
-from chartapp.models import Product, Contact
+from chartapp.models import Product, Contact, ContactInteraction
+
 
 class MyAdminSite(AdminSite):
     # Text to put at the end of each page's <title>.
@@ -16,11 +17,23 @@ class MyAdminSite(AdminSite):
 
 class ContactAdmin(admin.ModelAdmin):
     fields = ["last_name", "first_name"]
+    list_display = ["last_name", "first_name", 'about', ]
     def response_change(self, request, obj):
         pass
 
+class ContactInteractionAdmin(admin.ModelAdmin):
+    list_display = ["id", "show_firm_url","name", "contact", "contacts"]
+    list_filter = []
+    search_fields = []
+    list_editable = [ "name"]
 
+    def show_firm_url(self, obj):
+        return '<a href="%s%s">%s</a>' % ('http://url-to-prepend.com/', obj.url_field, obj.url_field)
+
+    show_firm_url.allow_tags = True
+    show_firm_url.short_description = 'Column description'
 
 
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(ContactInteraction, ContactInteractionAdmin)
 admin.site.register(Product)
--- chartapp/models.py
+++ chartapp/models.py
@@ -20,8 +20,33 @@ class Contact(models.Model):
 
     email = models.EmailField()
 
+    id = models.IntegerField(auto_created=True, primary_key=True)
+
     def __str__(self):
         return '{}{}'.format(
             self.first_name,
             self.last_name,
         )
+
+    @property
+    def about(self):
+        return -1
+
+
+class ContactInteraction(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=255)
+    url_field = models.URLField(max_length=255)
+
+    @property
+    def contact(self):
+        return Contact.objects.get(first_name="qqq")
+
+    @property
+    def contacts(self):
+        return models.ForeignKey(
+            Contact,
+            on_delete=models.CASCADE,
+            related_name='last_name'
+        )
+
